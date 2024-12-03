280d0f45f9a5c307a038c2c617cc100c110d2ebf
valentina <valentina@Debian.myguest.virtualbox.org>
2024-12-03 19:14:45 +0100

    Added the new functions

--- network.py
+++ network.py
@@ -60,7 +60,40 @@ class Network(object):
                         for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta / len(mini_batch)) * nb 
                        for b, nb in zip(self.weights, nabla_b)]
-        
+
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        activation = x
+        activations = [x] # list to show all the activations, layer by layer
+        zs = [] #liste to show all the z vectros
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        delta = self.cost_dervative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+    def eavluate(self, test_data):
+        test_results = [(np.argmax(self.feed_forward(x)), y) for x, y in test_data]
+        return sum(int( x == y) for x, y in test_results)
+    
+    def cost_derivative(self, output_activations, y):
+        return output_activations-y
+
 
 def sigmoid(z):
-    return 1.0/(1.0+ np.exp(-z))
+    return 1.0/(1.0+ np.exp(-z))  
+
+def sigmoid_prime(z):
+    return sigmoid(z) * (1 - sigmoid(z))
